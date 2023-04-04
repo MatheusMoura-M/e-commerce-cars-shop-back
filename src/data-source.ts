93ad4e1a4abdd70c1a1bd75e0d6f6ab4f36afadb
leo@DESKTOP-H3DCF6T <leonardoneveswork@gmail.com>
2023-04-04 20:05:37 -0300

    build: build project base

---
+++ src/data-source.ts
@@ -0,0 +1,40 @@
+import path from "path";
+import "reflect-metadata"
+import { DataSourceOptions, DataSource } from "typeorm";
+import "dotenv/config"
+
+const DataSourceSettings =  (): DataSourceOptions => {
+
+    const entities   = [path.join(__dirname, "src/entities/**.{js,ts}")]
+    const migrations = [path.join(__dirname, "src/migrations/**.{js,ts}")]
+
+    const node_env = process.env.NODE_ENV
+
+    if(node_env === "production"){
+        return{
+            type: "postgres",
+            url: process.env.DATABASE_URL,
+            entities,
+            migrations
+        }
+    }
+
+    return {
+        type: "postgres",
+        username: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        port: +process.env.DB_PORT,
+        host: process.env.DB_HOST,
+        database: process.env.DB_NAME,
+        synchronize: false,
+        logging: true,
+        entities,
+        migrations
+    }
+
+}
+
+const dataSource = DataSourceSettings()
+const appDataSource = new DataSource(dataSource)
+
+export default appDataSource
---
+++ src/server.ts
@@ -0,0 +1,12 @@
+import app from "./app";
+import appDataSource from "./data-source";
+
+appDataSource.initialize().then(() => {
+
+    console.log("server connected")
+
+    app.listen(3000, () => {
+        console.log("server in running on port 3000");
+    })
+
+})
