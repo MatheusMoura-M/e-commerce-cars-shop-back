335287f638e4f054b0af67689c37914a74c02b6a
leo@DESKTOP-H3DCF6T <leonardoneveswork@gmail.com>
2023-04-05 10:41:08 -0300

    add: add cors

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import "express-async-errors"
 import express from "express"
 import { handleError } from "./error/appError.error"
+import cors from "cors"
 
 const app = express()
 app.use(express.json())
 
 app.use(handleError)
+app.use(cors())
 
 export default app
