308cc41dde829ff9e51a57855c12632dc3d40595
Matheus M <matheus150101miranda@gmail.com>
2023-04-05 13:56:05 -0300

    add libs and folders base

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,40 +1,38 @@
 import path from "path";
-import "reflect-metadata"
+import "reflect-metadata";
 import { DataSourceOptions, DataSource } from "typeorm";
-import "dotenv/config"
+import "dotenv/config";
 
-const DataSourceSettings =  (): DataSourceOptions => {
+const DataSourceSettings = (): DataSourceOptions => {
+  const entities = [path.join(__dirname, "src/entities/**.{js,ts}")];
+  const migrations = [path.join(__dirname, "src/migrations/**.{js,ts}")];
 
-    const entities   = [path.join(__dirname, "src/entities/**.{js,ts}")]
-    const migrations = [path.join(__dirname, "src/migrations/**.{js,ts}")]
+  const node_env = process.env.NODE_ENV;
 
-    const node_env = process.env.NODE_ENV
-
-    if(node_env === "production"){
-        return{
-            type: "postgres",
-            url: process.env.DATABASE_URL,
-            entities,
-            migrations
-        }
-    }
-
+  if (node_env === "production") {
     return {
-        type: "postgres",
-        username: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        port: +process.env.DB_PORT,
-        host: process.env.DB_HOST,
-        database: process.env.DB_NAME,
-        synchronize: false,
-        logging: true,
-        entities,
-        migrations
-    }
+      type: "postgres",
+      url: process.env.DATABASE_URL,
+      entities,
+      migrations,
+    };
+  }
 
-}
+  return {
+    type: "postgres",
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    port: +process.env.DB_PORT,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    synchronize: false,
+    logging: true,
+    entities,
+    migrations,
+  };
+};
 
-const dataSource = DataSourceSettings()
-const appDataSource = new DataSource(dataSource)
+const dataSource = DataSourceSettings();
+const appDataSource = new DataSource(dataSource);
 
-export default appDataSource
+export default appDataSource;
