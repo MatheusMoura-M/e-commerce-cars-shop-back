12863f084fd6066867627a0e080aa59bdf00467a
netomilfont <netomilfont@hotmail.com>
2023-04-10 23:44:01 -0300

    feat: add service to update a car

--- src/controllers/car/updateCar.controller.ts
+++ src/controllers/car/updateCar.controller.ts
@@ -1,8 +1,10 @@
 import { Request, Response } from "express";
+import { updateCarService } from "../../services/car/updateCar.service";
 
 export const updateCarController = async (req: Request, res: Response) => {
   const carUpdateData = req.body;
   const userId: string = req.id;
-  const carUpdated = updateCarService(carUpdateData, userId);
+  const carId: string = req.params.id;
+  const carUpdated = await updateCarService(carUpdateData, userId, carId);
   return res.status(200).json(carUpdated);
 };
---
+++ src/services/car/updateCar.service.ts
@@ -0,0 +1,38 @@
+import AppDataSource from "../../data-source";
+import { Car } from "../../entities/car.entity";
+import { User } from "../../entities/user.entity";
+import { AppError } from "../../error/appError.error";
+
+export const updateCarService = async (
+  carUpdateData,
+  userId: string,
+  carId: string
+) => {
+  const userRepository = AppDataSource.getRepository(User);
+  const carRepository = AppDataSource.getRepository(Car);
+
+  const user = await userRepository.findOneBy({
+    id: userId,
+  });
+
+  const car = await carRepository.findOneBy({
+    id: carId,
+  });
+
+  if (!car) {
+    throw new AppError("Car not found!", 404);
+  }
+
+  if (car.user.id !== user!.id) {
+    throw new AppError("You don't have permission to update this car", 403);
+  }
+
+  const updatedCar = carRepository.create({
+    ...car,
+    ...carUpdateData,
+  });
+
+  await carRepository.save(updatedCar);
+
+  return updatedCar;
+};
