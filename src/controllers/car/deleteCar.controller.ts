bbaa918f9326854b64f3182f4c6ac53c3d273135
netomilfont <netomilfont@hotmail.com>
2023-04-12 11:10:29 -0300

    feat: add controller to delete a car

---
+++ src/controllers/car/deleteCar.controller.ts
@@ -0,0 +1,8 @@
+import { Request, Response } from "express";
+
+export const deleteCarController = async (req: Request, res: Response) => {
+  const userId: string = req.id;
+  const carId: string = req.params.id;
+  const carDeleted = await deleteCarService(userId, carId);
+  return res.status(204).json(carDeleted);
+};
