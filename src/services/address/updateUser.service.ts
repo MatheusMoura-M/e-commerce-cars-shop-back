27cc47023d510a6757c9468d61a53b6929020a47
Matheus M <matheus150101miranda@gmail.com>
2023-04-28 12:20:10 -0300

    add routes address

--- src/services/address/updateUser.service.ts
+++ src/services/address/updateUser.service.ts
@@ -11,6 +11,25 @@ export const updateAddressService = async (
   id: string,
   payload: iAddressUpdateRequest
 ): Promise<iAddressUpdateResponse> => {
+  if (payload.city === "") {
+    delete payload.city;
+  }
+  if (payload.complement === "") {
+    delete payload.complement;
+  }
+  if (payload.number === "") {
+    delete payload.number;
+  }
+  if (payload.state === "") {
+    delete payload.state;
+  }
+  if (payload.street === "") {
+    delete payload.street;
+  }
+  if (payload.zipcode === "") {
+    delete payload.zipcode;
+  }
+
   const userFound = await userRepo.findOne({
     where: {
       id: id,
